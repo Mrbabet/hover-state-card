551c6c6b1e01ac3a69158acdd4c3e1e6e3260187
Mrbabet <wieliczkokamil@gmail.com>
2023-01-01 21:10:11 +0100

    Fix: script works now when clicking the corresponding card

--- script.js
+++ script.js
@@ -4,16 +4,29 @@ let cardThree = document.getElementById("cardThree");
 
 let buttonActive = false;
 
-document.addEventListener("click", function () {
+cardOne.addEventListener("click", function () {
   if (!buttonActive) {
     cardOne.classList.add("active");
-    cardTwo.classList.add("active");
-    cardThree.classList.add("active");
-
     buttonActive = true;
   } else {
     cardOne.classList.remove("active");
+    buttonActive = false;
+  }
+});
+cardTwo.addEventListener("click", function () {
+  if (!buttonActive) {
+    cardTwo.classList.add("active");
+    buttonActive = true;
+  } else {
     cardTwo.classList.remove("active");
+    buttonActive = false;
+  }
+});
+cardThree.addEventListener("click", function () {
+  if (!buttonActive) {
+    cardThree.classList.add("active");
+    buttonActive = true;
+  } else {
     cardThree.classList.remove("active");
     buttonActive = false;
   }
